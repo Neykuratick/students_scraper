2ed796ebe6343db85826141cd9d38f59bf4c54af
Neykuratick <neykuratick@yahoo.com>
2022-05-31 13:13:58 +0300

    upd

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from database.deals_crud import db
 from jobs.amocrm import run_deals
@@ -7,8 +9,9 @@ scheduler = AsyncIOScheduler(timezone='UTC', daemon=True)
 scheduler.add_job(run_deals, 'interval', [db], seconds=3)
 scheduler.start()
 
+asyncio.get_event_loop().run_forever()
 
-# TODO unhardcode amocrm refresh token
+
 # TODO admin report message
 # TODO move student's card in the AMOCRM pipeline according to the current application status
 
