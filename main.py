156eca5176ea51c6069bf92e05e377bff1344a15
Neykuratick <neykuratick@yahoo.com>
2022-06-01 11:04:00 +0300

    refactor

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import asyncio
-
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from database.deals_crud import db
-from jobs.amocrm import run_deals
+from amo_crm.job import run_deals
+from mpgu.job import store_deals
 
 scheduler = AsyncIOScheduler(timezone='UTC', daemon=True)
-# scheduler.add_job(store_deals, 'interval', [db], seconds=3)
-scheduler.add_job(run_deals, 'interval', [db], seconds=3)
+scheduler.add_job(store_deals, 'interval', [db], seconds=3)
+# scheduler.add_job(run_deals, 'interval', [db], seconds=3)
 scheduler.start()
 
 asyncio.get_event_loop().run_forever()
